5bb55bf0d02f58e8cbafa87713e9f932954e74a3
nitinkumarpals <nitinpal900@gmail.com>
2025-02-02 21:12:41 +0530

    fix: added @types/express to resolve TypeScript declaration issue

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 import { client } from "@repo/db/client";
 const app = express();
 const port = 3000;
-app.user(express.json());
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const user = await client.user.create({
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,16 +1,16 @@
 import { WebSocketServer } from "ws";
 import { client } from "@repo/db/client";
 
-const server = new WebSocketServer({ port: 3001 });
+const server = new WebSocketServer({
+  port: 3001,
+});
 
-server.on("connection", (ws) => {
-  ws.on("message", async (message) => {
-    const user = await client.user.create({
-      data: {
-        email: Math.random().toString(),
-        password: Math.random().toString(),
-      },
-    });
-    ws.send("hello");
+server.on("connection", async (socket) => {
+  client.user.create({
+    data: {
+      username: Math.random().toString(),
+      password: Math.random().toString(),
+    },
   });
+  socket.send("Hi there you are connected to the server");
 });
